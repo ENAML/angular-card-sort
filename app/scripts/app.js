0f1659311e40571df0a882d5f0dad128925cfd6b
ENAML <EthanACline@gmail.com>
2014-10-10 10:14:49 -0400

    angular sorts grid into 2 columns 1 row

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'angular.filter'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -16,4 +17,31 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .filter('rowSort', function(){
+    return function(groups, columns) {
+
+      var rows = [];
+      var rowCounter = 0;
+      rows[rowCounter] = [];
+
+      var columnCounter = 0;
+
+      for (var i = 0; i < groups.length; i++) {
+        if (columnCounter < columns) {
+          rows[rowCounter].push(groups[i]);
+          columnCounter++;
+        } else {
+          rowCounter++;
+          rows[rowCounter] = [];
+          rows[rowCounter].push(groups[i]);
+          columnCounter = 0;
+          columnCounter++;
+
+        }
+      }
+
+
+      return rows;
+    };
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,15 @@
 angular.module('codeTestApp')
   .controller('MainCtrl', function ($scope,$http) {
 
-    $scope.groups = {};
+
+    // TODO: Separate groups into Angular Service (?)
+    $scope.groups = {}; //hashtable version
+    $scope.groupsArray = []; // need for filtering into rows
+    $scope.groupsSortedByRow = [];
+    $scope.columns = 2; // max number of columns per row;
+
+
+    // necessary for column filter
 
     // get data from JSON file
     $http.get('front_end_demo.json').success(function(json){
@@ -24,15 +32,68 @@ angular.module('codeTestApp')
       }
       console.log($scope.groups);
 
+      // puts groups into groupsArray
+      for (var group in $scope.groups) {
+        $scope.groupsArray.push($scope.groups[group]);
+      }
+
+      console.log($scope.groupsArray);
+
+      // Sorting into rows
+
+      var rowCounter = 0;
+      $scope.groupsSortedByRow[rowCounter] = [];
+
+      var columnCounter = 0;
+
+      for (var i = 0; i < $scope.groupsArray.length; i++) {
+        if (columnCounter < $scope.columns) {
+          $scope.groupsSortedByRow[rowCounter].push($scope.groupsArray[i]);
+          columnCounter++;
+        } else {
+          rowCounter++;
+          $scope.groupsSortedByRow[rowCounter] = [];
+          $scope.groupsSortedByRow[rowCounter].push($scope.groupsArray[i]);
+          columnCounter = 0;
+          columnCounter++;
+
+        }
+      }
+
+      console.log($scope.groupsSortedByRow);
+
     }).error(function() {
       console.log('uh oh');
     });
 
+
+
     // moves item in one of the groups from previous location to top/front
-    $scope.toTop = function(item, arr) {
-      var i = arr.indexOf(item);
-      arr.splice(i,1);
-      arr.unshift(item);
+    $scope.toTop = function(item, group) {
+      var i = group.indexOf(item);
+      group.splice(i,1);
+      group.unshift(item);
+    };
+
+
+    // TODO: separate modal into it's own controller
+
+    $scope.modal = {
+      show: false,
+      name: '',
+      subType: '',
+      shortDescription: ''
+    };
+
+    $scope.openModal = function(item) {
+      $scope.modal.name = item.name;
+      $scope.modal.subType = item.subType;
+      $scope.modal.shortDescription = item.shortDescription;
+      $scope.modal.show = true;
+    };
+
+    $scope.closeModal = function() {
+      $scope.modal.show = false;
     };
 
   });
